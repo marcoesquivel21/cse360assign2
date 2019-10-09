4a56411deca96724114e69485e53957e4cb42628
marcoesquivel <marcoesquivel@192.168.0.201>
2019-10-08 21:47:30 -0700

    hello

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,42 @@
+/**
+ * @author marcoesquivel
+ * @class ID LC7
+ * @assignment 2
+ */
+
+package cse360assign2;
+
+
+public class AddingMachine {
+	
+	private int total;
+	
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	public int getTotal () {
+		return total;
+	}
+	
+	public void add (int value) {
+		return 1;
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public String toString () {
+		return "";
+	}
+	
+	public void clear() {
+		
+		
+	}
+	
+
+}
